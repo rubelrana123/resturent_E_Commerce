43786c449784dfcac35cdd2b10c91518296e9eaf
rubelrana123 <rubelranatpi707458@gmail.com>
2025-02-14 22:40:07 +0600

    banner image thumb centerlize

--- resturent-front-end/src/Pages/Home/Banner/Banner.jsx
+++ resturent-front-end/src/Pages/Home/Banner/Banner.jsx
@@ -1,6 +1,5 @@
 
-
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
 
 import caroselImg1 from "../../../assets/home/04.jpg";
@@ -10,49 +9,35 @@ import caroselImg3 from "../../../assets/home/02.jpg";
 import caroselImg4 from "../../../assets/home/01.jpg";
 import caroselImg5 from "../../../assets/home/06.png";
 import caroselImg6 from "../../../assets/home/05.png";
-
+import './Banner.css'
 const Banner = () => {
     return (
-        <div>
-        <Carousel   
-        
-        showArrows={true}  
-        transitionTime={1000}
-        thumbWidth={100}
-        centerSlidePercentage={true} 
-        showStatus={false}  
-        infiniteLoop={true} 
-        autoPlay={true} 
-        interval={3000}  
-        showThumbs={true} 
-           >
-                <div>
-                    <img src={caroselImg1} />
-                </div>
-                <div>
-                    <img src={caroselImg2} />
-                  
-                </div>
-                <div>
-                    <img src={caroselImg3} />
-                </div>
-              
-                <div>
-                    <img src={caroselImg4} />
-                </div>
-                <div>
-                    <img src={caroselImg5} />
-                  
-                </div>
-            
-                <div>
-                    <img src={caroselImg6} />
-                  
-                </div>
-            
-              
-            </Carousel>
-        </div>
+       <div className=''>
+         <Carousel 
+         autoPlay={true}
+         infiniteLoop={true}
+         showStatus = {false}
+         >
+            <div>
+                <img src={caroselImg1} />
+            </div>
+            <div>
+                <img src={caroselImg2} />
+            </div>
+            <div>
+                <img src={caroselImg3} />
+            </div>
+            <div>
+                <img src={caroselImg4} />
+            </div>
+            <div>
+                <img src={caroselImg5} />
+            </div>
+            <div>
+                <img src={caroselImg6} />
+            </div>
+        </Carousel>
+       </div>
     );
 };
 
