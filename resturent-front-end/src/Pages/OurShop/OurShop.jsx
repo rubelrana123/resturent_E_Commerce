79833301bd8f898a1c778b7a94ddd66e4c742828
rubelrana123 <rubelranatpi707458@gmail.com>
2025-03-09 23:48:12 +0600

    add sibtitle

--- resturent-front-end/src/Pages/OurShop/OurShop.jsx
+++ resturent-front-end/src/Pages/OurShop/OurShop.jsx
@@ -1,10 +1,11 @@
  import bannerImage from "../../assets/shop/banner2.jpg"
 import SectionBanner from "../../components/Reuseable/SectionBanner/SectionBanner";
-
+import Subtitle from "../../components/Reuseable/Subtitle/Subtitle"
 const OurShop = () => {
     return (
         <div>
             <SectionBanner title = "Our Shop"  subtitle = "lets start your favourite" bannerImage = {bannerImage} ></SectionBanner>
+             <Subtitle heading ="our shop" subheading = "hello shop"></Subtitle>
         </div>
     );
 };
